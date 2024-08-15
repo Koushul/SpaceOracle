5f9119e2f127cc033ca875f4a66e163f94112007
Ally <alw399@pitt.edu>
2024-08-15 13:02:02 -0400

    finish sweeps

--- src/spaceoracle/tools/sweep.sh
+++ src/spaceoracle/tools/sweep.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=sweep_cnn
+#SBATCH --job-name=sweep_vit
 #SBATCH --gres=gpu:1
 #SBATCH --partition=preempt
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2-00:00:00 
-#SBATCH --output=sweep_cnn.txt
+#SBATCH --output=sweep_vit.txt
 
 cd ../../
 
 source activate bee
-# python -m spaceoracle.tools.sweep_vit
-python -m spaceoracle.tools.sweep_cnn
+python -m spaceoracle.tools.sweep_vit
+# python -m spaceoracle.tools.sweep_cnn
 
 
